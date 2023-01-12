4c0d1c209fc30bc5ea1d64551e452b099a94738b
Vinicius <viny0012@gmail.com>
2023-01-11 21:58:35 -0300

    material assincrono 11/01/2023

---
+++ js_Funcoes_Nativas/script.js
@@ -0,0 +1,8 @@
+let linkProz = document.getElementById("link-proz")
+let btnSubmit = document.querySelector("button[type=submit]")
+
+btnSubmit.addEventListener("click", (e)=> {
+    alert("Não foi possível acessar os dados do formulário")
+    e.preventDefault()
+    //alert("Não foi possível acessar o link");
+})
